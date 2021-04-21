6d955002ff1af60351c46963d68d6a854d42582f
memoer <hanjn2842@naver.com>
2021-04-21 23:04:07 +0900

    01-자료구조-기초 복습 완료, 02-자료구조-핵심__1874 까지 복습함 / 1966부터 시작하면 됨

--- fc_solving_problems/02-기본-자료구조-핵심/1874-r.py
+++ fc_solving_problems/02-기본-자료구조-핵심/1874-r.py
@@ -1,18 +1,29 @@
-# 복습
 n = int(input())
-count = 1
 stack = []
 result = []
+last_num = 0
+
+
+def add(n: int):
+    stack.append(n)
+    result.append("+")
+
+
+def pop():
+    stack.pop()
+    result.append("-")
+
+
 for _ in range(n):
-    data = int(input())
-    while data >= count:
-        stack.append(count)
-        count += 1
-        result.append("+")
-    if data == stack[-1]:
-        stack.pop()
-        result.append("-")
+    m = int(input())
+    if m > last_num:
+        for i in range(last_num + 1, m + 1):
+            add(i)
+        last_num = i
+    if stack[-1] == m:
+        pop()
     else:
         print("NO")
         exit(0)
+
 print("\n".join(result))
